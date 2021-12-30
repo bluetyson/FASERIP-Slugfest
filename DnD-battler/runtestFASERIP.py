02d3856bbc04ea1a488959c2198b971dc3d2ccb2
Richard Scott <Richard.Scott@ozminerals.com>
2021-12-31 10:17:31 +1030

    combatant typo in summary

--- DnD-battler/runtestFASERIP.py
+++ DnD-battler/runtestFASERIP.py
@@ -9,16 +9,16 @@ billbybob2 = Creature.load('Rat Pack')
 #level1 = Creature.load('Guard Dog')
 billbybob2 = Creature.load('Cat')
 
-#level1 = Creature.load('Amazing Martial Artist')
+level1 = Creature.load('Amazing Martial Artist')
 billbybob2 = Creature.load('Lawyer')
 billbybob3 = Creature.load('Lawyer')
-#billbybob4 = Creature.load('Plumber')
-billbybob5 = Creature.load('Plumber')
-billbybob6 = Creature.load('Plumber')
-billbybob7 = Creature.load('Plumber')
-billbybob8 = Creature.load('Plumber')
-billbybob9 = Creature.load('Plumber')
-billbybob10 = Creature.load('Plumber')
+billbybob4 = Creature.load('Boxer')
+billbybob5 = Creature.load('Boxer')
+billbybob6 = Creature.load('Boxer')
+billbybob7 = Creature.load('Boxer')
+billbybob8 = Creature.load('Boxer')
+billbybob9 = Creature.load('Boxer')
+billbybob10 = Creature.load('Boxer')
 
 #print(billbybob2.alignment, billbybob2.starting_hp, billbybob2.hp)
 #billbybob2.alignment = "green dragon evil"
@@ -29,18 +29,18 @@ billbybob10 = Creature.load('Plumber')
 #arena2 = Encounter(level1).addmobFASERIP(10)
 #arena2 = Encounter(level1).addmobFASERIP(6)
 #arena2 = Encounter(level1).addmobFASERIP(2)
-#arena2 = Encounter(level1, billbybob4, billbybob5, billbybob6, billbybob7, billbybob8, billbybob9, billbybob10)
+arena2 = Encounter(level1, billbybob4, billbybob5, billbybob6, billbybob7, billbybob8, billbybob9, billbybob10)
 #print(arena2)
 
 #arena3 = Encounter(level1).addmobFASERIP(1)
 arena3 = Encounter(Creature.load('Amazing Martial Artist')).addmobFASERIP(1)
-print("AddMob Martial Artist v Plumber")
+print("AddMob Martial Artist v Boxer")
 
-billbybob4 = Creature.load('Plumber')
+billbybob4 = Creature.load('Boxer')
 level1 = Creature.load('Amazing Martial Artist')
 
 arena2 = Encounter(level1, billbybob4)
-print("Encounter Martial Artist v Plumber")
+print("Encounter Martial Artist v Boxer")
 
 
 #print(arena3.go_to_war(1))
@@ -66,6 +66,22 @@ for x in arena3.__dict__:
 print(arena2.go_to_war(1))
 print(arena3.go_to_war(1))
 
+billbybob15 = Creature.load('Boxer', alignment="mob")
+level15 = Creature.load('Amazing Martial Artist')
+arena4 = Encounter(billbybob15, level15)
+#print(arena4.go_to_war(1))
+
+billbybob16 = Creature.load('Boxer', alignment="mob")
+level16 = Creature.load('Amazing Martial Artist')
+arena5 = Encounter(level16, billbybob16)
+#print(arena5.go_to_war(1000))
+
+
+arena2 = Encounter(level1, billbybob4, billbybob5, billbybob6, billbybob7, billbybob8, billbybob9, billbybob10)
+arena21 = Encounter(level1, billbybob4, billbybob5, billbybob6, billbybob7, billbybob8, billbybob9)
+print(arena21.go_to_war(1000))
+
+
 ## notes on running
 #load_beastiary from csv
 
@@ -88,3 +104,8 @@ print(arena3.go_to_war(1))
 #add martial arts in
 #power fun
 #then random character gen? - java things
+
+#add group initiative
+
+
+
