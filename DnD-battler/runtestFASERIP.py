62ebee2d45d3bdebaf0cbc3f0c4888d099b4e1b4
Richard Scott <Richard.Scott@ozminerals.com>
2021-12-30 23:11:47 +1030

    odd error with addmob and things the wrong way round

--- DnD-battler/runtestFASERIP.py
+++ DnD-battler/runtestFASERIP.py
@@ -1,18 +1,18 @@
 from DnD_battler import Creature, Encounter
 
-level1 = Creature.load('Cat')
+#level1 = Creature.load('Cat')
 billbybob2 = Creature.load('Rat')
 #level1 = Creature.load('Scientist')
 #billbybob2 = Creature.load('Lawyer')
-level1 = Creature.load('Lawyer')
+#level1 = Creature.load('Lawyer')
 billbybob2 = Creature.load('Rat Pack')
-level1 = Creature.load('Guard Dog')
+#level1 = Creature.load('Guard Dog')
 billbybob2 = Creature.load('Cat')
 
-level1 = Creature.load('Amazing Martial Artist')
+#level1 = Creature.load('Amazing Martial Artist')
 billbybob2 = Creature.load('Lawyer')
 billbybob3 = Creature.load('Lawyer')
-billbybob4 = Creature.load('Plumber')
+#billbybob4 = Creature.load('Plumber')
 billbybob5 = Creature.load('Plumber')
 billbybob6 = Creature.load('Plumber')
 billbybob7 = Creature.load('Plumber')
@@ -20,20 +20,51 @@ billbybob8 = Creature.load('Plumber')
 billbybob9 = Creature.load('Plumber')
 billbybob10 = Creature.load('Plumber')
 
-print(billbybob2.alignment, billbybob2.starting_hp, billbybob2.hp)
+#print(billbybob2.alignment, billbybob2.starting_hp, billbybob2.hp)
 #billbybob2.alignment = "green dragon evil"
-arena2 = Encounter(level1, billbybob4, billbybob2)
+#arena2 = Encounter(level1, billbybob4, billbybob2)
 #print(arena2)
 #print(arena2.battle())
 #print(arena2.go_to_war(100))
 #arena2 = Encounter(level1).addmobFASERIP(10)
 #arena2 = Encounter(level1).addmobFASERIP(6)
 #arena2 = Encounter(level1).addmobFASERIP(2)
-arena2 = Encounter(level1, billbybob4, billbybob5, billbybob6, billbybob7, billbybob8, billbybob9, billbybob10)
-print(arena2)
+#arena2 = Encounter(level1, billbybob4, billbybob5, billbybob6, billbybob7, billbybob8, billbybob9, billbybob10)
+#print(arena2)
+
+#arena3 = Encounter(level1).addmobFASERIP(1)
+arena3 = Encounter(Creature.load('Amazing Martial Artist')).addmobFASERIP(1)
+print("AddMob Martial Artist v Plumber")
+
+billbybob4 = Creature.load('Plumber')
+level1 = Creature.load('Amazing Martial Artist')
+
+arena2 = Encounter(level1, billbybob4)
+print("Encounter Martial Artist v Plumber")
+
+
+#print(arena3.go_to_war(1))
+#print(arena2.go_to_war(1))
 #print(arena2.go_to_war(100))
-print(arena2.go_to_war(1000))
+#print(arena2.go_to_war(1000))
 #print(arena2.go_to_war(10000))
+print(arena2)
+print(arena3)
+
+#print(dir(arena2))
+#print(dir(arena3))
+
+assert dir(arena2) == dir(arena3)
+
+for x in arena2.__dict__:
+	print(x, arena2.__dict__[x])
+	
+for x in arena3.__dict__:
+	print(x, arena3.__dict__[x])
+	
+	
+print(arena2.go_to_war(1))
+print(arena3.go_to_war(1))
 
 ## notes on running
 #load_beastiary from csv
