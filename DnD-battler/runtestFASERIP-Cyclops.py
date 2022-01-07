92961e77fcee31b07bd4844e31bb7ee2901b6a74
Richard Scott <Richard.Scott@ozminerals.com>
2022-01-07 17:08:50 +1030

    Cyclops vs Corsair

--- DnD-battler/runtestFASERIP-Cyclops.py
+++ DnD-battler/runtestFASERIP-Cyclops.py
@@ -3,17 +3,15 @@ import pandas as pd
 
 arenaDangerRoom = Encounter(Creature.load("Cyclops"), Creature.load("Corsair"))
 
-
-#print(arenaKunLun.set_deathmatch())
-print(arenaDangerRoom.go_to_war(1))
-#print(arenaClobbering.go_to_war(10))
-#print(arenaClobbering.go_to_war(100))
-#print(arenaClobbering.go_to_war(500))
-#print(arenaClobbering.go_to_war(1000))
-#print(arenaClobbering.go_to_war(2000))
-#print(arenaClobbering.go_to_war(5000))
-#print(arenaClobbering.go_to_war(10000))
-#print(arenaKunLun.go_to_war(100000))
+#print(arenaDangerRoom.go_to_war(1))
+#print(arenaDangerRoom.go_to_war(10))
+#print(arenaDangerRoom.go_to_war(100))
+#print(arenaDangerRoom.go_to_war(500))
+#print(arenaDangerRoom.go_to_war(1000))
+#print(arenaDangerRoom.go_to_war(2000))
+#print(arenaDangerRoom.go_to_war(5000))
+print(arenaDangerRoom.go_to_war(10000))
+#print(arenaDangerRoom.go_to_war(5000))
 
 #print(arenaEarly.go_to_war(1))
 
