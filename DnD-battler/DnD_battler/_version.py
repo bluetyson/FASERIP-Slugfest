1108cda2a00b0ccdcdd78a1114bf2c8830bd1231
Blue Tyson <bluetyson@users.noreply.github.com>
2022-01-15 14:25:44 +1030

    email typo

--- DnD-battler/DnD_battler/_version.py
+++ DnD-battler/DnD_battler/_version.py
@@ -1,6 +1,6 @@
 __author__ = "Blue Tyson"
 __copyright__ = "Don't blame me if evil WINS"
 __licence___ = 'MIT'
-__email__ = "aussieavmp2@gmail.com"
+__email__ = "aussievamp2@gmail.com"
 __date__ = '15/01/22'
 __version__ = '0.1'
