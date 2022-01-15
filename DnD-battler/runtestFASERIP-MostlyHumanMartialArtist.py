9e10bb604f39d25204968eb54523b740b16e1b25
Richard Scott <Richard.Scott@ozminerals.com>
2022-01-15 17:38:16 +1030

    fix a couple of relative paths

--- DnD-battler/runtestFASERIP-MostlyHumanMartialArtist.py
+++ DnD-battler/runtestFASERIP-MostlyHumanMartialArtist.py
@@ -6,7 +6,7 @@ level1 = Creature.load('Amazing Martial Artist')
 #arena215 = Encounter(level1, billbybob4, billbybob5, billbybob6, billbybob7, billbybob8, billbybob9, billbybob15, billbybob16, billbybob17, billbybob18, billbybob19, billbybob20, billbybob21, billbybob22, billbybob23)
 arenaKunLun = Encounter()
 
-MHMA = pd.read_csv(r'J:\CLONE\WORK2\FASERIP-Slugfest\Mostly-Human-Martial-Arts-All.csv')
+MHMA = pd.read_csv('../Mostly-Human-Martial-Arts-All.csv')
 print(MHMA)
 for index, row in MHMA.iterrows():
 	#if 'Shang' in 
