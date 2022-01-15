2c2649c69c60884914a4b1e01d02e99b7f018240
Blue Tyson <bluetyson@users.noreply.github.com>
2022-01-15 14:28:07 +1030

    encounter base typos

--- DnD-battler/DnD_battler/encounter/__init__.py
+++ DnD-battler/DnD_battler/encounter/__init__.py
@@ -3,7 +3,8 @@ from ._action import EncounterAction # inherits EncounterBase
 class Encounter(EncounterAction):
     """
     The encounter class is the arena for the battle
-    In a dimentionless model, move action and the main actions dash, disengage, hide, shove back/aside, tumble and overrun are meaningless.
+    TODO: update this for FASERIP to detail how things work
+    In a dimensionless model, move action and the main actions dash, disengage, hide, shove back/aside, tumble and overrun are meaningless.
     weapon attack —default
     two-weapon attack —
         Good when the opponent has low AC (<12) if 2nd attack is without proficiency.
@@ -11,7 +12,7 @@ class Encounter(EncounterAction):
         Due to the 1 action for donning/doffing a shield, switch to two handed is valid for unshielded folk only.
         Best keep two weapon fighting as a prebuild not a combat switch.
     AoE spell attack — Layout…
-    targetted spell attack —produce flame is a cantrip so could be written up as a weapon. The bigger ones. Spell slots need to be re-written.
+    targeted spell attack —produce flame is a cantrip so could be written up as a weapon. The bigger ones. Spell slots need to be re-written.
     spell buff —Barkskin is a druidic imperative. Haste? Too much complication.
     spell debuff —Bane…
     dodge —targetted and turn economy
